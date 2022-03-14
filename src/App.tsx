bb867be79eb5a3458a11b1185c20df7c616f7931
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-14 00:08:48 -0700

    Adding react router, page background, and Market component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Market from './Components/Market';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='header'>metaMarket</h1>
     </div>
   );
 }
