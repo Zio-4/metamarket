b19e9c99ee6f3359310a2623ee145229d4bf5699
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-18 12:28:40 -0700

    Adding auth, storage, and API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "metamarket": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "listingImage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "metamarket": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
