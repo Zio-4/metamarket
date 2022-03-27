dedd07493fb3be66a5dba92435a100a972259585
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-27 00:08:21 -0700

    Adding some things to cart component

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <Grid container justifyContent='center' sx={{marginTop: 2}}>
+      <Grid item>
+        <Typography sx={{color: 'white', fontSize: '1.8rem'}}>
+          Subtotal: $20.36
+        </Typography>
+        <Button variant='outlined' sx={{color: "#66FCF1", marginTop: 1}}>
+          Proceed to checkout
+        </Button>
+      </Grid>
+      <Grid item>
+        <Card>
+          <CardContent>
+            
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 
