99ca1b2bad2af5e3baba51384147c28c760196c4
Philip Ziolkowski <philipz4848@gmail.com>
2022-04-02 13:20:33 +0200

    Adding authenticator to login component

--- src/Components/User/Login.tsx
+++ src/Components/User/Login.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function Login() {
   return (
-    <div>Login</div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   )
 }
 
