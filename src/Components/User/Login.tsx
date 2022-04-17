d773cef963f3b803c74e28cf86c506e2086c89c7
Philip Ziolkowski <philipz4848@gmail.com>
2022-04-16 23:09:15 -0700

    Adding AWS imports in index.tsx

--- src/Components/User/Login.tsx
+++ src/Components/User/Login.tsx
@@ -7,7 +7,7 @@ function Login() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user.username}</h1>
+          <h1 className='placeholder-text'>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
