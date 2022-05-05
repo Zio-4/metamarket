99f959f7fe46d26b6ae5af76beef8b8fdd29258d
Philip Ziolkowski <philipz4848@gmail.com>
2022-05-05 16:29:23 -0700

    Designed basic forms for signing in and creating an account

--- src/Components/User/Login.tsx
+++ src/Components/User/Login.tsx
@@ -1,9 +1,18 @@
 import React, {useState } from 'react'
-import { Tab, Box, TextField, Button} from '@mui/material'
+import { Tab, Box, TextField, Button, Typography} from '@mui/material'
 import { TabPanel, TabContext, TabList } from '@mui/lab';
 
 function Login() {
   const [tabValue, setTabValue] = useState("1")
+  const [signInFormValues, setSignInFormValues] = useState({
+    username: '',
+    password: ''
+  })
+  const [createAccountFormValues, setCreateAccountFormValues] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
     setTabValue(newValue)
@@ -12,17 +21,27 @@ function Login() {
   return (
     <div>
       <TabContext value={tabValue}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', bgcolor: 'gray',}}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', bgcolor: 'white',}}>
           <TabList onChange={handleTabChange} aria-label="login & sign up tabs" textColor='secondary' indicatorColor='secondary' centered >
             <Tab label="Sign in" value="1" />
-            <Tab label="Sign Up" value="2" />
+            <Tab label="Create Account" value="2" />
           </TabList>
         </Box>
-        <TabPanel value="1" sx={{bgcolor: 'white'}}>
-          <TextField variant='outlined'/>
+        <TabPanel value="1" sx={{bgcolor: '#C5C6C7'}}>
+          <TextField variant='outlined' label='Username' sx={{ width: '100%', }}/>
+          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem' }}/>
+          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}}>
+            Sign In
+          </Button>
+          <Typography sx={{textAlign: 'center', marginTop: '0.5rem'}}>Forgot your password?</Typography>
         </TabPanel>
-        <TabPanel value="2">
-          Item Two
+        <TabPanel value="2" sx={{bgcolor: '#C5C6C7'}}>
+          <TextField variant='outlined' label='Username' sx={{ width: '100%', }}/>
+          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem',}}/>
+          <TextField variant='outlined' label='Confirm Password' sx={{ width: '100%', marginTop: '0.5rem', }}/>
+          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}}>
+            Create Account
+          </Button>
         </TabPanel>
       </TabContext>
     </div>
