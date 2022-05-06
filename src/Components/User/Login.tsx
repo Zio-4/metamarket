dd03985df53c2235b4a98053f5f371c1c9e28334
Philip Ziolkowski <philipz4848@gmail.com>
2022-05-05 19:57:24 -0700

    Adding event listeners to sign in and create account fields

--- src/Components/User/Login.tsx
+++ src/Components/User/Login.tsx
@@ -2,20 +2,51 @@ import React, {useState } from 'react'
 import { Tab, Box, TextField, Button, Typography} from '@mui/material'
 import { TabPanel, TabContext, TabList } from '@mui/lab';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 function Login() {
   const [tabValue, setTabValue] = useState("1")
   const [signInFormValues, setSignInFormValues] = useState({
-    username: '',
-    password: ''
+    signInUsername: '',
+    signInPassword: ''
   })
   const [createAccountFormValues, setCreateAccountFormValues] = useState({
-    username: '',
-    password: '',
-    confirmPassword: ''
+    createAccountUsername: '',
+    createAccountPassword: '',
+    createAccountConfirmPassword: '',
+    createAccountEmail: ''
   })
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
     setTabValue(newValue)
+  }
+
+  const handleSignInChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignInFormValues({...signInFormValues, [e.target.name]: e.target.value})
+  }
+
+  const handleCreateAccountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCreateAccountFormValues({...createAccountFormValues, [e.target.name]: e.target.value})
+  }
+
+  const signInUser = () => {
+    // Call Auth API with credentials
+    setSignInFormValues({
+      signInUsername: '',
+      signInPassword: ''
+    })
+  }
+
+  const createAccount = () => {
+    // Call Auth API
+    // console.log(`${createAccountFormValues.createAccountUsername} ${createAccountFormValues.createAccountPassword} ${createAccountFormValues.createAccountConfirmPassword} ${createAccountFormValues.createAccountEmail}`)
+    setCreateAccountFormValues({
+      createAccountUsername: '',
+      createAccountPassword: '',
+      createAccountConfirmPassword: '',
+      createAccountEmail: ''
+    })
   }
 
   return (
@@ -28,18 +59,19 @@ function Login() {
           </TabList>
         </Box>
         <TabPanel value="1" sx={{bgcolor: '#C5C6C7'}}>
-          <TextField variant='outlined' label='Username' sx={{ width: '100%', }}/>
-          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem' }}/>
-          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}}>
+          <TextField variant='outlined' label='Username' sx={{ width: '100%', }} name='signInUsername' value={signInFormValues.signInUsername} onChange={handleSignInChange}/>
+          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem' }} name='signInPassword' value={signInFormValues.signInPassword} onChange={handleSignInChange}/>
+          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}} onClick={signInUser}>
             Sign In
           </Button>
           <Typography sx={{textAlign: 'center', marginTop: '0.5rem'}}>Forgot your password?</Typography>
         </TabPanel>
         <TabPanel value="2" sx={{bgcolor: '#C5C6C7'}}>
-          <TextField variant='outlined' label='Username' sx={{ width: '100%', }}/>
-          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem',}}/>
-          <TextField variant='outlined' label='Confirm Password' sx={{ width: '100%', marginTop: '0.5rem', }}/>
-          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}}>
+          <TextField variant='outlined' label='Username' sx={{ width: '100%', }} name='createAccountUsername' value={createAccountFormValues.createAccountUsername} onChange={handleCreateAccountChange}/>
+          <TextField variant='outlined' label='Password' sx={{ width: '100%', marginTop: '0.5rem',}} name='createAccountPassword' value={createAccountFormValues.createAccountPassword} onChange={handleCreateAccountChange}/>
+          <TextField variant='outlined' label='Confirm Password' sx={{ width: '100%', marginTop: '0.5rem', }} name='createAccountConfirmPassword' value={createAccountFormValues.createAccountConfirmPassword} onChange={handleCreateAccountChange}/>
+          <TextField variant='outlined' label='Email' sx={{ width: '100%', marginTop: '0.5rem', }} name='createAccountEmail' value={createAccountFormValues.createAccountEmail} onChange={handleCreateAccountChange}/>
+          <Button variant='contained' sx={{ width: '100%', marginTop: '0.8rem'}} onClick={createAccount}>
             Create Account
           </Button>
         </TabPanel>
