a6bea87431e6dccc83e9e41a263cfb052c9f9a61
Philip Ziolkowski <philipz4848@gmail.com>
2022-06-05 22:11:49 -0700

    Adding Stripe as a dependency for Lambda function

--- amplify/backend/function/createStripeConnectAccount/src/index.js
+++ amplify/backend/function/createStripeConnectAccount/src/index.js
@@ -1,13 +1,12 @@
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB({apiVersion: '2012-10-08'});
 const stripe = require('stripe')('sk_test_51J22KpGBmWPuX4VCT8wPqbVnX3yTKaGsuQXOIb5mbOKUZ3h03V0HNDsK9tPfWNCa6jtGwNFJWvt5H5EXVDImNOo10015VuHc6l');
 
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
 
 
 exports.handler = async (event) => {
